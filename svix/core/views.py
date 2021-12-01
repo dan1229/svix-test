09b06daf09acc81959547d5dfd52cb90f03f0c12
danielnazarian <daniel@thinknimble.com>
2021-12-01 09:24:51 -0500

    basic view setup

--- svix/core/views.py
+++ svix/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from svix.api import Svix, ApplicationIn
+
+
+def index(request):
+
+    svix = Svix("AUTH_TOKEN")
+    app = svix.application.create(ApplicationIn(name="Application name"))
+
+    # good overview https://api.svix.com/docs#section/Introduction
+    return render(request, "index.html")
