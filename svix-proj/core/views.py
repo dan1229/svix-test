824e7255cc8e535e37ab46bb9e13dbed646c8d31
danielnazarian <daniel@thinknimble.com>
2021-12-02 10:33:46 -0500

    only open dash

--- svix-proj/core/views.py
+++ svix-proj/core/views.py
@@ -14,29 +14,30 @@ def index(request):
     svix = Svix(os.environ.get('SVIX_API_KEY'))
     
     
-    # good overview https://api.svix.com/docs#section/Introduction
-    # an "application" refers to a user application
-    # i.e., where we will be sending webhooks
-    app = svix.application.create(ApplicationIn(name=app_id, uid=app_id))
+    # # good overview https://api.svix.com/docs#section/Introduction
+    # # an "application" refers to a user application
+    # # i.e., where we will be sending webhooks
+    # app = svix.application.create(ApplicationIn(name=app_id, uid=app_id))
 
     
-    # sending messages
-    # https://www.svix.com/guides/sending/send-webhooks-with-svix-cli/
-    svix.message.create(
-    app_id,
-    MessageIn(
-        event_type="invoice.paid",
-        event_id="djydntynd",
-        payload={
-            "id": "djydj",
-            "status": "paid",
-            "attempt": 2
-            }
-        )
-    )
+    # # sending messages
+    # # https://www.svix.com/guides/sending/send-webhooks-with-svix-cli/
+    # svix.message.create(
+    # app_id,
+    # MessageIn(
+    #     event_type="invoice.paid",
+    #     event_id="djydntynd",
+    #     payload={
+    #         "id": "djydj",
+    #         "status": "paid",
+    #         "attempt": 2
+    #         }
+    #     )
+    # )
     
     # ui dashboard
     dashboard_access_out = svix.authentication.dashboard_access(app_id)
-    # print(dashboard_access_out)
-    # decoded = json.loads(dashboard_access_out)
+    
+    # redirect to dashboard
+    # NOTE: on initial usage you will have to setup a webhook endpoint
     return redirect(dashboard_access_out.url)
